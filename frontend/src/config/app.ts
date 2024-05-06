28983d32a0a569145302b9ac14a05e0d457dac45
kre8mymedia <kre8mymedia@gmail.com>
2024-05-06 16:36:36 -0500

    Update the URL or static

--- frontend/src/config/app.ts
+++ frontend/src/config/app.ts
@@ -1,3 +1,3 @@
 
 export const ON_PREM = process.env.NEXT_PUBLIC_ON_PREM === "true";
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.adaptive.biz";
