cfaddd6971a9fd6def9da3827740729a27481b25
kre8mymedia <kre8mymedia@gmail.com>
2024-06-25 10:21:54 -0500

    if update build fails do not proceed

--- backend/scripts/update.sh
+++ backend/scripts/update.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -e
 
 SERVICE="api"
 
+# Build the service
 docker-compose build $SERVICE
+
+# Stop the service if it is running
 docker-compose stop $SERVICE
+
+# Start the service without its dependencies
 docker-compose up -d --no-deps $SERVICE
