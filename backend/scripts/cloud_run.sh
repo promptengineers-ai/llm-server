1049a992ec825ecc6a4eeebc4f8bf183dbe81dfb
kre8mymedia <kre8mymedia@gmail.com>
2024-06-26 00:02:19 -0500

    listen on 8080

---
+++ backend/scripts/cloud_run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -a # automatically export all variables
+source $ENV_FILE
+set +a
+
+
+echo "Starting Cloud Run Server"
+VARS="REDIS_URL=$REDIS_URL,DATABASE_URL=$DATABASE_URL,OPENAI_API_KEY=$OPENAI_API_KEY,GROQ_API_KEY=$GROQ_API_KEY,ANTHROPIC_API_KEY=$ANTHROPIC_API_KEY"
+
+gcloud run deploy llm-server \
+    --image docker.io/promptengineers/llm-server:cb064a4 \
+    --platform managed \
+    --region us-central1 \
+    --allow-unauthenticated \
+    --set-env-vars $VARS
