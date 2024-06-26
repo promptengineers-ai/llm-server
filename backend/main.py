c94073971c9a7abcbe159d8808ed234cf83cefc0
kre8mymedia <kre8mymedia@gmail.com>
2024-06-25 23:30:20 -0500

    Adds default user

--- backend/main.py
+++ backend/main.py
@@ -4,4 +4,4 @@ import asyncio
 from src.config import APP_ENV, APP_WORKERS
 
 if __name__ == "__main__":
-	uvicorn.run("src.api:app", port=8080, reload=APP_ENV == "local", workers=APP_WORKERS)
+	uvicorn.run("src.api:app", host="0.0.0.0", port=8080, reload=APP_ENV == "local", workers=APP_WORKERS)
