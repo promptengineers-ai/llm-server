473b4e1ec5a961643ea955a84f6695512a10f507
kre8mymedia <kre8mymedia@gmail.com>
2024-06-28 21:20:51 -0500

    Stop pulling rom ini file

--- backend/scripts/cloud_run.sh
+++ backend/scripts/cloud_run.sh
@@ -4,14 +4,15 @@ set -a # automatically export all variables
 source .env
 set +a
 
+SHORT_SHA=$(git rev-parse --short HEAD)
 
 echo "Starting Cloud Run Server"
 VARS="REDIS_URL=$REDIS_URL,DATABASE_URL=$DATABASE_URL,OPENAI_API_KEY=$OPENAI_API_KEY,GROQ_API_KEY=$GROQ_API_KEY,ANTHROPIC_API_KEY=$ANTHROPIC_API_KEY"
 echo "VARS: $VARS"
 
-# gcloud run deploy llm-server \
-#     --image docker.io/promptengineers/llm-server:fc7ae1d \
-#     --platform managed \
-#     --region us-central1 \
-#     --allow-unauthenticated \
-#     --set-env-vars $VARS
+gcloud run deploy llm-server \
+    --image docker.io/promptengineers/llm-server:$SHORT_SHA \
+    --platform managed \
+    --region us-central1 \
+    --allow-unauthenticated \
+    --set-env-vars $VARS
