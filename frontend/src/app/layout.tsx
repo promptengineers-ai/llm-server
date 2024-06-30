73aafa0855c1f1b4a337c59d443d0d227581dfd1
kre8mymedia <kre8mymedia@gmail.com>
2024-06-29 21:45:03 -0500

    Forntend updates

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import 'highlight.js/styles/github-dark-dimmed.min.css';
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/contexts/ThemeContext";
@@ -26,7 +27,60 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark-dimmed.css"></link>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, maximum-scale=1"
+                />
+                <link
+                    href="splashscreens/iphone5_splash.png"
+                    media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/iphone6_splash.png"
+                    media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/iphoneplus_splash.png"
+                    media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/iphonex_splash.png"
+                    media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/iphonexr_splash.png"
+                    media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/iphonexsmax_splash.png"
+                    media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/ipad_splash.png"
+                    media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/ipadpro1_splash.png"
+                    media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/ipadpro3_splash.png"
+                    media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)"
+                    rel="apple-touch-startup-image"
+                />
+                <link
+                    href="splashscreens/ipadpro2_splash.png"
+                    media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)"
+                    rel="apple-touch-startup-image"
+                />
             </head>
             <body className={inter.className}>
                 <ThemeProvider>
